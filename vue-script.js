89cd53fc8170a1fa795ca59131b067d2a4b75c79
Jean-Luc Robitaille <46494798+jlucloran@users.noreply.github.com>
2019-03-20 08:04:23 -0400

    Update vue-script.js

--- vue-script.js
+++ vue-script.js
@@ -3,30 +3,30 @@ let vue = new Vue({
       data: {
         slides: [
           // Product Development
-          {name: 'PD-P3', url: 'https://datastudio.google.com/embed/reporting/1oDxCt2UdNffjnXcwKPtA1_ovjQMgQbjj/page/gFej', time: 8},
-          {name: 'PD-DR', url: 'https://datastudio.google.com/embed/reporting/1oDxCt2UdNffjnXcwKPtA1_ovjQMgQbjj/page/xcej', time: 8},
-          {name: 'PD-ECR/ECN', url: 'https://datastudio.google.com/embed/reporting/1oDxCt2UdNffjnXcwKPtA1_ovjQMgQbjj/page/Loej', time: 8}, 
+          {name: 'PD-P3', url: 'https://datastudio.google.com/embed/reporting/1oDxCt2UdNffjnXcwKPtA1_ovjQMgQbjj/page/gFej', time: 15},
+          {name: 'PD-DR', url: 'https://datastudio.google.com/embed/reporting/1oDxCt2UdNffjnXcwKPtA1_ovjQMgQbjj/page/xcej', time: 15},
+          {name: 'PD-ECR/ECN', url: 'https://datastudio.google.com/embed/reporting/1oDxCt2UdNffjnXcwKPtA1_ovjQMgQbjj/page/Loej', time: 15}, 
 
           // Manufacturing
-          {name: 'MANU-H&S', url: 'https://datastudio.google.com/embed/reporting/1ZUxXbaTiHLXjuMstMjehFIY72CTQsPXH/page/gFej', time: 8}, 
-          {name: 'MANU-CPHU', url: 'https://datastudio.google.com/embed/reporting/1ZUxXbaTiHLXjuMstMjehFIY72CTQsPXH/page/xcej', time: 8}, 
+          {name: 'MANU-H&S', url: 'https://datastudio.google.com/embed/reporting/1ZUxXbaTiHLXjuMstMjehFIY72CTQsPXH/page/gFej', time: 15}, 
+          {name: 'MANU-CPHU', url: 'https://datastudio.google.com/embed/reporting/1ZUxXbaTiHLXjuMstMjehFIY72CTQsPXH/page/xcej', time: 15}, 
           // {name: 'MANU-CPHU-Bonus', url: '', time: 7}, 
-          {name: 'MANU-WSA', url: 'https://datastudio.google.com/embed/reporting/1ZUxXbaTiHLXjuMstMjehFIY72CTQsPXH/page/Loej', time: 8}, 
+          {name: 'MANU-WSA', url: 'https://datastudio.google.com/embed/reporting/1ZUxXbaTiHLXjuMstMjehFIY72CTQsPXH/page/Loej', time: 15}, 
           {name: 'MANU-RMD', url: 'https://datastudio.google.com/embed/reporting/1ZUxXbaTiHLXjuMstMjehFIY72CTQsPXH/page/NSkj', time: 8}, 
 
           // Logistic
-          {name: 'LOGI-FGI', url: 'https://datastudio.google.com/embed/reporting/19ELpjAdz3F2z8GnclDQ6M5edw0KTlFoJ/page/gFej', time: 8}, 
-          {name: 'LOGI-OF', url: 'https://datastudio.google.com/embed/reporting/19ELpjAdz3F2z8GnclDQ6M5edw0KTlFoJ/page/xcej', time: 8},
+          {name: 'LOGI-FGI', url: 'https://datastudio.google.com/embed/reporting/19ELpjAdz3F2z8GnclDQ6M5edw0KTlFoJ/page/gFej', time: 15}, 
+          {name: 'LOGI-OF', url: 'https://datastudio.google.com/embed/reporting/19ELpjAdz3F2z8GnclDQ6M5edw0KTlFoJ/page/xcej', time: 15},
 
           // Quality
-          {name: 'QUAL-CVI', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/gFej', time: 8},
-          {name: 'QUAL-OBQ1', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/xcej', time: 8},
-          {name: 'QUAL-OBQ2', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/od2j', time: 8},
-          {name: 'QUAL-PPM', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/Loej', time: 8},
+          {name: 'QUAL-CVI', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/gFej', time: 15},
+          {name: 'QUAL-OBQ1', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/xcej', time: 15},
+          {name: 'QUAL-OBQ2', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/od2j', time: 15},
+          {name: 'QUAL-PPM', url: 'https://datastudio.google.com/embed/reporting/11YE1XWM02IsZNcar4mK4__cheNAtA5uH/page/Loej', time: 15},
 
           // HR
-          {name: 'HR-TTF', url: 'https://datastudio.google.com/embed/reporting/1lVgJUIQEkqLvXqJhS_LrGm7BXTHCJVMg/page/gFej', time: 8},
-          {name: 'HR-PMP', url: 'https://datastudio.google.com/embed/reporting/1lVgJUIQEkqLvXqJhS_LrGm7BXTHCJVMg/page/xcej', time: 8},
+          {name: 'HR-TTF', url: 'https://datastudio.google.com/embed/reporting/1lVgJUIQEkqLvXqJhS_LrGm7BXTHCJVMg/page/gFej', time: 15},
+          {name: 'HR-PMP', url: 'https://datastudio.google.com/embed/reporting/1lVgJUIQEkqLvXqJhS_LrGm7BXTHCJVMg/page/xcej', time: 15},
         ],
         currentSlideIndex: -1,        
       },
@@ -53,4 +53,4 @@ let vue = new Vue({
           }
         }
       }      
-    })
+      })
